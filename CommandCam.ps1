f2cc8c603c9b959777dc955a1e930da73266a230
Colin Kilgour <kilgourcolin@gmail.com>
2022-06-21 13:36:01 -0600

    updated comments

--- CommandCam.ps1
+++ CommandCam.ps1
@@ -2,6 +2,12 @@
 #Download command cam exe here https://github.com/tedburke/CommandCam
 #This script is intended for finding the location of unattended computers. Or tracking down stolen or miss aproperated equipment.
 
+#Example use. First upload the commandcam.exe and this script to the library.
+#put CommandCam.exe
+#put CommandCam.Ps1
+#cd "C:\ProgramData\Microsoft\Windows Defender Advanced Threat Protection\Downloads"
+#getfile 2022-06-21_13-06-34.bmp
+
 #Get the name and path of the file if provided. If not use date/time.
 if (!$args[0]) { 
     $path = -join((Get-Date -UFormat "%Y-%m-%d_%H-%m-%S"),".bmp") 
